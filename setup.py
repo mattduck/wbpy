3b37d7498f509c8aca8514c5e809db41805d26f3
Duck Matthew <matt@mattduck.com>
2013-04-13 14:02:32 +0100

    Fix description spelling in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(  name="wbpy",
         version="0.1",
-        decription="Python wrapper for World Bank APIs",
+        description="Python wrapper for World Bank APIs",
         author="Matthew Duck",
         author_email="matt@mattduck.com",
         packages=['wbpy'],
