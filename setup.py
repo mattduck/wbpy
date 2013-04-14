ad4963a2f93db6a063a3451ceacb25f0c3a70090
Duck Matthew <matt@mattduck.com>
2013-04-14 12:36:28 +0100

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from distutils.core import setup
 
 setup(  name="wbpy",
         version="0.1",
-        description="Python wrapper for World Bank APIs",
+        license="MIT",
+        description="A simpler interface to the World Bank APIs",
+        long_description=open('README.rst').read(),
         author="Matthew Duck",
         author_email="matt@mattduck.com",
+        url="https://github.com/mattduck/wbpy",
         packages=['wbpy'],
      )
