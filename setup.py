5ba35b9a0b33e7970e4bc58c0b66beabc2f79c7e
Duck Matthew <matt@mattduck.com>
2013-10-09 23:10:18 +0100

    Add some metadata to __init__.py

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
+import re
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+init_py = open("wbpy/__init__.py").read()
+METADATA = dict(re.findall("__([a-z]+)__ = \"([^\"]+)\"", init_py))
+
 setup(  
-    name="wbpy",
-    version="1.0.0",
-    license="MIT",
+    name=METADATA["name"],
+    version=METADATA["version"],
+    license=METADATA["license"],
     author="Matthew Duck",
-    author_email="matt@mattduck.com",
+    author_email=METADATA["email"],
     description="A Python interface to the World Bank Indicators and Climate APIs",
     long_description=open('README.rst').read(),
     url="https://github.com/mattduck/wbpy",
     packages=['wbpy'],
     provides=['wbpy'],
+    package_data={"wbpy": ["non_ISO_region_codes.json"]},
     install_requires=['pycountry'],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Intended Audience :: Developers', 
-                 'Intended Audience :: Science/Research', 
-                 'License :: OSI Approved :: MIT License',
-                 'Operating System :: OS Independent', 
-                 'Programming Language :: Python', 
-                 'Topic :: Scientific/Engineering', 
-                ]
- )
+    classifiers=[
+        'Development Status :: 4 - Beta',
+         'Intended Audience :: Developers', 
+         'Intended Audience :: Science/Research', 
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent', 
+         'Programming Language :: Python', 
+         'Topic :: Scientific/Engineering', 
+        ]
+    )
--- wbpy/__init__.py
+++ wbpy/__init__.py
@@ -1,6 +1,12 @@
 from indicators import IndicatorAPI, IndicatorDataset
 from climate import ClimateAPI, InstrumentalDataset, ModelledDataset
 
+__name__ = "wbpy"
+__version__ = "1.0.0"
+__email__ = "matt@mattduck.com"
+__maintainer__ = "Matthew Duck"
+__license__ = "MIT"
+
 __all__ = [
     IndicatorAPI,
     IndicatorDataset,
