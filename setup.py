f74295fbaded50b8fdb8c618ef6f59386c17f511
Duck Matthew <matt@mattduck.com>
2013-10-28 18:00:06 +0000

    Add python 3 classifier

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
          'License :: OSI Approved :: MIT License',
          'Operating System :: OS Independent', 
          'Programming Language :: Python', 
+         'Programming Language :: Python 3', 
          'Topic :: Scientific/Engineering', 
         ],
     **kw)
